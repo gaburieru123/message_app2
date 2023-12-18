8bc55d28b1504d02aef3adcee1ff8e640ce87979
gaburieru123 <dari1tena@gmail.com>
2023-12-18 19:28:08 +0900

    git reset後のコミット。タスクをメッセージに、保存を送信に変更した。メッセージ保存機能追加。サインアップ画面への遷移リンク追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_16_025435) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_17_094723) do
+  create_table "messages", force: :cascade do |t|
+    t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
