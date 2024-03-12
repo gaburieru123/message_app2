81c70fd05b389ed13e583714891deb5b21f41d75
gaburieru123 <dari1tena@gmail.com>
2024-03-12 23:03:45 +0900

    Revert "ユーザー編集機能追加。"
    This reverts commit ee4f765ce18d84f2cf52efea77bea180d5cc7f63.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,15 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_current_user
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email])
   end
 
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
-
-  
-  def set_current_user
-    if session[:user_id].present?
-      @current_user = User.find_by(id: session[:user_id])
-    else
-      @current_user = nil
-    end
-    # @current_user = User.find_by(id: session[:user_id])
-  end
-
-  
 
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,18 +2,20 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!, except: [:new]
 
   def index
+    @user = User.find(1)
+    @sent_messages = @user.sent_messages.order(created_at: :desc)
   end
   
   def new
   end
   
   def create
-    message = Message.new(message: params[:message], sender_id: current_user.id, recipient_id: params[:recipient_id])
-      if message.save
+    @message = Message.new(message: params[:message], sender_id: current_user.id, recipient_id: params[:recipient_id])
+      if @message.save
         flash[:notice] = "メッセージ作成に成功しました"
         redirect_to "/user/#{params[:id]}"
       else
-        flash[:error] = message.errors.full_messages
+        flash[:error] = @message.errors.full_messages
         render :new, status: :unprocessable_entity
       end
   end
