da7a3673c2189d8c54014c5760157497f4189bc0
gaburieru123 <dari1tena@gmail.com>
2024-03-16 16:30:32 +0900

    メッセージ編集機能のアクセス制限追加。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,6 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!, except: [:new]
-
-  def index
-  end
+  before_action :correct_user, only: [:edit, :update]
   
   def new
   end
@@ -11,10 +9,41 @@ class MessagesController < ApplicationController
     message = Message.new(message: params[:message], sender_id: current_user.id, recipient_id: params[:recipient_id])
       if message.save
         flash[:notice] = "メッセージ作成に成功しました"
-        redirect_to "/user/#{params[:id]}"
+        redirect_to "/users/#{message.recipient_id}"
       else
         flash[:error] = message.errors.full_messages
         render :new, status: :unprocessable_entity
       end
   end
+
+  def edit
+    @message = Message.find(params[:id])
+  end
+
+  def update
+    message = Message.find(params[:id])
+    if message.update(edit_message_params)
+      flash[:notice] = "メッセージ編集に成功しました"
+      redirect_to "/users/#{message.recipient_id}"
+    else
+      flash[:error] = message.errors.full_messages
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def edit_message_params
+    params.require(:message).permit(:message)
+  end
+
+  def correct_user
+    msg = Message.find(params[:id])
+    if user_signed_in? && msg.sender_id == current_user.id
+    else 
+      flash[:notice] = "無効な操作です。"
+      redirect_to root_path
+    end
+  end
+
 end
